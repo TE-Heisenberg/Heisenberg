1306437f386b91619184985e9b1edd1596b13f60
sonam8991 <sonam.rajput1@wipro.com>
2016-05-09 04:50:10 +0000

    Update

--- public/js/controller/itineraryController.js
+++ public/js/controller/itineraryController.js
@@ -2,138 +2,47 @@
  * Created by user on 02-05-2016.
  */
 angular.module('app').controller("itineraryController",function($scope) {
-    var node1={
-        final:{
-            city:{
-                name:"bangalore"
-            },
-            local:[
-                {
-                    logo:"public/assets/images/uber.png",
-                    type:"taxi",
-                    source:"koramangala",
-                    destination:"bangalore airport",
-                    pickUpDate:"10-05-2016",
-                    pickUpTime:"10:00",
-                    dropDate:"10-05-2016",
-                    dropTime:"14:00",
-                    price:"1000"
-                }
-            ]
-        }
-    }
-    var edge1={
-        final: {
-            flight: [
-                {
-                    logo: "public/assets/images/indigo.png",
-                    name: "indigo",
-                    source: "bangalore",
-                    destination: "delhi",
-                    startDate: "10-05-2016",
-                    endDate: "10-05-2016",
-                    startTime: "16:00",
-                    endTime: "20:00",
-                    sourceAirport: "bangalore airport",
-                    destinationAirport: "delhi airport",
-                    class: "economic",
-                    noOfStop:"3h travelling time (0 stop)",
-                    preference: [
-                        {
-                            meal: "veg",
-                            baggage: "15 kg/person"
-                        }
-                    ],
-                    price: 37000
-                }
-            ]
-        }
-    }
-    var node2={
-        final:{
-            city:{
-                name:"delhi"
-            },
-            local:[
-                {
-                    logo:"public/assets/images/uber.png",
-                    type:"taxi",
-                    source:"airport",
-                    destination:"hotel taj",
-                    pickUpDate:"10-05-2016",
-                    pickUpTime:"20:30",
-                    dropDate:"10-05-2016",
-                    dropTime:"23:00",
-                    price:1200
-                },
-                {
-                    logo:"public/assets/images/ola.png",
-                    type:"taxi",
-                    source:"airport",
-                    destination:"hotel taj",
-                    pickUpDate:"10-05-2016",
-                    pickUpTime:"20:30",
-                    dropDate:"10-05-2016",
-                    dropTime:"23:00",
-                    price:1200
-                },
-                {
-                    logo:"public/assets/images/uber.png",
-                    type:"taxi",
-                    source:"airport",
-                    destination:"hotel taj",
-                    pickUpDate:"10-05-2016",
-                    pickUpTime:"20:30",
-                    dropDate:"10-05-2016",
-                    dropTime:"23:00",
-                    price:1200
-                },
-                {
-                    logo:"public/assets/images/uber.png",
-                    type:"taxi",
-                    source:"airport",
-                    destination:"hotel taj",
-                    pickUpDate:"10-05-2016",
-                    pickUpTime:"20:30",
-                    dropDate:"10-05-2016",
-                    dropTime:"23:00",
-                    price:1200
-                }
-            ],
-            accommodation:[
-                {
-                    logo:"public/assets/images/taj.png",
-                    name:"hotel taj",
-                    rating:"*****",
-                    location:"nehru nagar",
-                    roomType:"AC room",
-                    checkInDate:"10-05-2016",
-                    checkInTime:"23:00",
-                    checkOutDate:"12-05-2016",
-                    checkOutTime:"16:00",
-                    price:8000
-                },
-                {
-                    logo:"public/assets/images/taj.png",
-                    name:"hotel taj",
-                    rating:"*****",
-                    location:"nehru nagar",
-                    roomType:"AC room",
-                    checkInDate:"10-05-2016",
-                    checkInTime:"23:00",
-                    checkOutDate:"12-05-2016",
-                    checkOutTime:"16:00",
-                    price:8000
-                }
-            ]
-        }
-    }
+   var travelPlanData=new Object();
+   var sequence=[];
+   var nodesData=new Object();
+   var edgesData=new Object();
+   var nodeObj=new Object();
+   var edgeObj=new Object();
+   var finalArray=[];
 
-    var array=[];
-    array.push(node1);
-    array.push(edge1);
-    array.push(node2);
-    $scope.finalArray=array;
+        $http.get('public/data/travelPlan.json').success(function(data) {
+         $scope.travelPlanData=data;
+          console.log($scope.travelPlanData);
+          fun();
+     });
 
+ var fun=function(){
+      sequence=$scope.travelPlanData['sequence'];
+      console.log(sequence);
+
+      nodesData=$scope.travelPlanData['nodes'];
+      console.log(nodesData);
+
+      edgesData=$scope.travelPlanData['edges'];
+      console.log(edgesData);
+
+     for(var i=0;i<sequence.length;i++){
+         console.log('hello '+sequence[i].substr(0,4));
+
+         if(sequence[i].substr(0,4)=='node') {
+             nodeObj = nodesData[sequence[i]];
+             finalArray.push(nodeObj);
+         }
+         else if(sequence[i].substr(0,4)=='edge'){
+             edgeObj = edgesData[sequence[i]];
+             finalArray.push(edgeObj);
+         }
+
+     }
+
+     console.log(finalArray);
+     $scope.jsonData=finalArray;
+
+ }
 
 });
