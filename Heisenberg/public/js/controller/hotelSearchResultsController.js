1befc7770c998be1a2e9a4af637f2438e4384181
lakshaybansal <blakshay93@gmail.com>
2016-05-11 15:30:39 +0530

    removed unneccessary commits

--- Heisenberg/public/js/controller/hotelSearchResultsController.js
+++
@@ -1,6 +0,0 @@
-angular.module('app')
-.controller('hotelSearchResultsController', function($scope, $http){
-	$http.get('public/data/hotelSearchResults.json').success(function(searchresults){
-		$scope.searchresults= searchresults;
-	});
-});
