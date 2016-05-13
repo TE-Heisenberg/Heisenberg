677f210a34a9484744cb1f6882943f231537e3f2
lakshaybansal <blakshay93@gmail.com>
2016-05-13 14:33:23 +0530

    Done the component routing and travel plan component

---
+++ public/components/landingPageComponent/landingPageComponent.js
@@ -0,0 +1,9 @@
+var app = angular.module("app").component("landingPageComponent", {
+
+    templateUrl: "public/components/landingPageComponent/landingPageComponent.html",
+    controllerAs: "landingPage",
+    controller: function(){
+
+    }
+
+})
---
+++ public/components/mainComponent/mainComponent.js
@@ -0,0 +1,30 @@
+angular.module('app')
+.component("mainComponent",{
+templateUrl:"public/components/mainComponent/mainComponent.html",
+controllerAs:"mainComponent",
+controller:["$rootScope","$location",mainComponentController],
+$routeConfig: [
+    {path: '/landingPage',name: 'LandingPageComponent', component: 'landingPageComponent'},
+    {path: '/travelBooking',name: 'TravelBookingComponent', component: 'travelBookingComponent' },
+    {path:'/stayBooking',name: 'StayBookingComponent', component: 'stayBookingComponent'},
+    {path:'/flightSearchResults',name: 'FlightSearchResultsComponent', component: 'flightSearchResultsComponent'},
+    {path:'/trainSearchResults',name:'TrainSearchResultsComponent',component:'trainSearchResultsComponent'},
+    {path:'/hotelSearchResults',name:'HotelSearchResultsComponent',component:'hotelSearchResultsComponent'},
+    {path:'/itinerary',name:'ItineraryComponent',component:'itineraryComponent'},
+    {path:'/**',redirectTo:["LandingPageComponent"]}
+  ]
+});
+function mainComponentController($rootScope,$location){
+  var mainComponent=this;
+      if ($location.path().indexOf('landingPage') > 0)
+      {
+        console.log("hello landing is there");
+          mainComponent.landingPage = true;
+          console.log(mainComponent.landingPage);
+      }
+      else {
+        console.log("hello");
+          mainComponent.landingPage = false;
+      }
+
+}
