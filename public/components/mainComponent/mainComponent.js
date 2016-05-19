cb5f57604c42a6403a80cc848189504a53774b6d
pranay06 <niruprao001@gmail.com>
2016-05-19 20:32:53 +0530

    Removed the dependency of services on inner components and implemented few components

--- public/components/mainComponent/mainComponent.js
+++ public/components/mainComponent/mainComponent.js
@@ -5,7 +5,7 @@ controllerAs:"mainComponent",
 controller:["$rootScope","$location",mainComponentController],
 $routeConfig: [
 
-    {path: '/landingPage',name: 'LandingPageComponent', component: 'landingPageComponent', bindings},
+    {path: '/landingPage',name: 'LandingPageComponent', component: 'landingPageComponent'},
     {path: '/travelBooking',name: 'TravelBookingComponent', component: 'travelBookingComponent' },
     {path:'/stayBooking',name: 'StayBookingComponent', component: 'stayBookingComponent'},
     {path:'/flightSearchResults',name: 'FlightSearchResultsComponent', component: 'flightSearchResultsComponent'},
@@ -17,89 +17,89 @@ $routeConfig: [
   ]
 });
 
-// var setObj = function(obj, keyString,value) {
-// 		console.log("Before Replace ", keyString)
-//     keyString = keyString.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-//     console.log("After first replace", keyString);
-//     keyString = keyString.replace(/^\./, '');           // strip a leading dot
-//     console.log("After second replace", keyString);
-//     var hierarchyWiseKeysArray = keyString.split('.');
-//
-//     while (hierarchyWiseKeysArray.length > 1)
-//         obj = obj[hierarchyWiseKeysArray.shift()];
-//     return obj[hierarchyWiseKeysArray.shift()] = value;
-//
-// }
+var setObj = function(obj, keyString,value) {
+		console.log("Before Replace ", keyString)
+    keyString = keyString.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+    console.log("After first replace", keyString);
+    keyString = keyString.replace(/^\./, '');           // strip a leading dot
+    console.log("After second replace", keyString);
+    var hierarchyWiseKeysArray = keyString.split('.');
+
+    while (hierarchyWiseKeysArray.length > 1)
+        obj = obj[hierarchyWiseKeysArray.shift()];
+    return obj[hierarchyWiseKeysArray.shift()] = value;
+
+}
 
 function mainComponentController($rootScope,$location){
-    //
-    //
-    //  mainComponent.travelPlanData = {};
-    //  mainComponent.travelPlanInitializer = function(indexForTravelMode) {
-    //    var modeOfTravel = ["oneWay", "twoWay", "multiWay"];
-    //    mainComponent.travelPlanData = {
-    //      "travelType": modeOfTravel[indexForTravelMode+1],
-    //      "state": "initial",
-    //      "nodes": {},
-    //      "edges": {}
-    //
-    //   };
-    //
-    //    var indexForNodesAndEdges = 0;
-    //    while(indexForNodesAndEdges < indexForTravelMode+1) {
-    //       mainComponent.nodeEdgeInitializer("nodes");
-    //       mainComponent.nodeEdgeInitializer("edges");
-    //    }
-    //
-    //    mainComponent.nodeEdgeInitializer("nodes");
-    //
-    //  }
-    //
-    //  mainComponent.nodeEdgeInitializer = function(nodeOrEdge)
-    //  {
-    //    var numberOfElements = Object.keys(mainComponent.travelPlanData[nodeOrEdge]).length;
-    //    var id = nodeOrEdge.slice(0, -1)+numberOfElements;
-    //    mainComponent.travelPlanData[nodeOrEdge][id] = {
-    //      "status": "initial"
-    //    };
-    //
-    //   //  mainComponent.nodeEdgeInitializer[nodeEdgeInitializer]
-    //  }
-    //
-    //
-    // mainComponent.insertInformationInTravelPlan = function(keyString, value)
-    // {
-    //   setObj(mainComponent.travelPlanData, keyString,value);
-    // }
-    //  mainComponent.insertNodeEdgeInfo = function(nodeOrEdge) {
-    //
-    //  }
-    //
-    //
-    //
-    //  mainComponent.childServicesInitializer  = function(nodeOrEdge) {
-    //
-    //  }
-    //
-    //  mainComponent.insertChildServicesData = function(nodeOrEdge) {
-    //
-    //  }
-    //
+
+    var mainComponent = this;
+     mainComponent.travelPlanData = {};
+     mainComponent.travelPlanInitializer = function(indexForTravelMode) {
+       var modeOfTravel = ["oneWay", "twoWay", "multiWay"];
+       mainComponent.travelPlanData = {
+         "travelType": modeOfTravel[indexForTravelMode+1],
+         "state": "initial",
+         "nodes": {},
+         "edges": {}
+
+      };
+
+       var indexForNodesAndEdges = 0;
+       while(indexForNodesAndEdges < indexForTravelMode+1) {
+          mainComponent.nodeEdgeInitializer("nodes");
+          mainComponent.nodeEdgeInitializer("edges");
+       }
+
+       mainComponent.nodeEdgeInitializer("nodes");
+
+     }
+
+     mainComponent.nodeEdgeInitializer = function(nodeOrEdge)
+     {
+       var numberOfElements = Object.keys(mainComponent.travelPlanData[nodeOrEdge]).length;
+       var id = nodeOrEdge.slice(0, -1)+numberOfElements;
+       mainComponent.travelPlanData[nodeOrEdge][id] = {
+         "status": "initial"
+       };
+
+      //  mainComponent.nodeEdgeInitializer[nodeEdgeInitializer]
+     }
+
+
+    mainComponent.insertInformationInTravelPlan = function(keyString, value)
+    {
+      setObj(mainComponent.travelPlanData, keyString,value);
+    }
+     mainComponent.insertNodeEdgeInfo = function(nodeOrEdge) {
+
+     }
+
+
+
+     mainComponent.childServicesInitializer  = function(nodeOrEdge) {
+
+     }
+
+     mainComponent.insertChildServicesData = function(nodeOrEdge) {
+
+     }
 
 
 
 
 
-     //  var mainComponent=this;
-       // if ($location.path().indexOf('landingPage') > 0)
-      // {
-      //   console.log("hello landing is there");
-      //     mainComponent.landingPage = true;
-      //     console.log(mainComponent.landingPage);
-      // }
-      // else {
-      //   console.log("hello");
-      //     mainComponent.landingPage = false;
-      // }
+
+      var mainComponent=this;
+       if ($location.path().indexOf('landingPage') > 0)
+      {
+        console.log("hello landing is there");
+          mainComponent.landingPage = true;
+          console.log(mainComponent.landingPage);
+      }
+      else {
+        console.log("hello");
+          mainComponent.landingPage = false;
+      }
 
 }
