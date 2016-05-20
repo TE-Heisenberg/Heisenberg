b893c282343ca3e5527d79224007b90460bcafff
pranay06 <niruprao001@gmail.com>
2016-05-20 19:33:24 +0530

    modified the signature of reflect functions

--- public/components/essentialFieldsFormComponent/essentialFieldsFormComponent.js
+++ public/components/essentialFieldsFormComponent/essentialFieldsFormComponent.js
@@ -2,7 +2,7 @@ angular.module('app')
 .component("essentialFieldsFormComponent",{
   templateUrl:"public/components/essentialFieldsFormComponent/essentialFieldsFormComponent.html",
   controllerAs:"essentialFieldsFormComponentCtrl",
-  controller:["$rootScope","$http","FetchService",essentialFieldsFormComponentCtrl],
+  controller:essentialFieldsFormComponentCtrl,
   bindings: {
     "formData":"<",
     "bindDataKey":"<",
