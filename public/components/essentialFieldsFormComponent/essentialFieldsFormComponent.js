71f53aca2419982d6466d2c70bb585b889f2bd60
pranay06 <niruprao001@gmail.com>
2016-05-20 19:16:47 +0530

    modified the signature of reflect functions

--- public/components/essentialFieldsFormComponent/essentialFieldsFormComponent.js
+++ public/components/essentialFieldsFormComponent/essentialFieldsFormComponent.js
@@ -1,23 +1,3 @@
-// var essentialFieldsFormComponentCtrl = function($http, $rootScope,FetchService) {
-//    var essentialFieldsFormComponentCtrl=this;
-//    console.log(" inside essential field forms");
-//    essentialFieldsFormComponentCtrl.$onInit = function () {
-//
-//              FetchService.nodeMaster().then(function (response) {
-//
-//                     essentialFieldsFormComponentCtrl.nodeMasterData = response;
-//                     console.log("node master");
-//                     console.log(essentialFieldsFormComponentCtrl.nodeMasterData);
-//              });
-//              FetchService.travelPlan().then(function (response) {
-//
-//                     essentialFieldsFormComponentCtrl.travelPlan = response;
-//              });
-//
-//     }
-// };
-
-
 angular.module('app')
 .component("essentialFieldsFormComponent",{
   templateUrl:"public/components/essentialFieldsFormComponent/essentialFieldsFormComponent.html",
@@ -48,17 +28,4 @@ function essentialFieldsFormComponentCtrl()
       essentialFieldsFormComponentCtrl.reflectFormData({"keyString":keyString,"value":value});
 
     };
-    var setObj = function(obj, keyString,value) {
-        console.log("Before Replace ", keyString)
-        keyString = keyString.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-        console.log("After first replace", keyString);
-        keyString = keyString.replace(/^\./, '');           // strip a leading dot
-        console.log("After second replace", keyString);
-        var hierarchyWiseKeysArray = keyString.split('.');
-
-        while (hierarchyWiseKeysArray.length > 1)
-            obj = obj[hierarchyWiseKeysArray.shift()];
-        return obj[hierarchyWiseKeysArray.shift()] = value;
-
-    }
 }
