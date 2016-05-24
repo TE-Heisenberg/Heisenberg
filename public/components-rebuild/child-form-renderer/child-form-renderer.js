7cba0075f0f70d2426ecb67d990e18651c1eec7f
Nishant Jain <nishantkumarajain@gmail.com>
2016-05-24 21:22:20 +0530

    components rebuild - created child form renderer

---
+++ public/components-rebuild/child-form-renderer/child-form-renderer.js
@@ -0,0 +1,32 @@
+angular.module('app')
+    .component('childFormRenderer', {
+       templateUrl: './public/components-rebuild/child-form-renderer/child-form-renderer.html',
+       controller: childFormRendererController,
+       controllerAs: "childFormRenderer",
+       bindings: {
+           fieldsData: '<',
+           fieldsMetaData: '<',
+           onAdd: '&',
+           onDelete: '&',
+           index: '@'
+       }
+    });
+    
+    function childFormRendererController() {
+        var childFormRenderer = this;
+        console.log(childFormRenderer);
+        // Reflecting the value changed in the custom input box
+        childFormRenderer.onReflect = function(keyString,value,id) {
+            childFormRenderer.fieldsData[id] = value;
+        };
+        
+        //Triggering the parent to add a new child-form-renderer
+        childFormRenderer.add = function() {
+            childFormRenderer.onAdd();
+        };
+        
+        // Triggering the parent to delete the current component, passing the index
+        childFormRenderer.delete = function() {
+            childFormRenderer.onDelete({'index': childFormRenderer.index});
+        };
+    };
