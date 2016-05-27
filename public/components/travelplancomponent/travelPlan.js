f7f3ae18d76bbfc3782ddfd1bb5bc9cf05e30b66
lakshay bansal <blakshay93@gmail.com>
2016-05-27 11:02:32 +0530

    Added routes and app.js express

--- public/components/travelplancomponent/travelPlan.js
+++ public/components/travelplancomponent/travelPlan.js
@@ -1,62 +1,56 @@
 /**
- * Created by lenovo on 09-05-2016.
- */
- angular.module("app").component("travelPlan", {
+* Created by lenovo on 09-05-2016.
+*/
+angular.module("app").component("travelPlan", {
 
-    templateUrl: "public/components/travelplancomponent/travelPlanDiv.html",
-    controllerAs: "plan",
-    controller: travelPlanController,
-    bindings: {
-        travelplanobject:'<',
-        currentnodeedge:'&',
-        nodetype:'@',
-        edgetype:'@'
-    }
+  templateUrl: "public/components/travelplancomponent/travelPlanDiv.html",
+  controllerAs: "plan",
+  controller: travelPlanController,
+  bindings: {
+    travelplanobject:'<',
+    currentnodeedgebooking:'&',
+    nodetype:'@',
+    edgetype:'@'
+  }
 }).filter('keylength', function () {
-    return function (input) {
-        if (!angular.isObject(input)) {
-            throw Error("Usage of non-objects with keylength filter!!")
-        }
-        return Object.keys(input).length;
+  return function (input) {
+    if (!angular.isObject(input)) {
+      throw Error("Usage of non-objects with keylength filter!!")
     }
+    return Object.keys(input).length;
+  }
 });
 
 function travelPlanController() {
 
-    var plan = this;
+  var plan = this;
 
-
-
-    //console.log(FetchService);
-
-
-
-    plan.currentnodeedgetravel = function (value) {
-      console.log(value);
-            plan.currentnodeedge(value);
-
+  plan.currentnodeedgetravel = function (value) {
+    console.log("in travel plan")
+    console.log(value);
+    plan.currentnodeedgebooking({value2:value});
+  };
+  plan.addNode = function () {
+    plan.newNode = {
+      type:"location",
+      cityName:" "
     };
-    plan.addNode = function () {
-          plan.newNode = {
-            type:"location",
-            cityName:" "
-          };
-            plan.newEdge = {
-              type:"transit",
-              childServices:
-              {
-                booking:{
-                  requested:{mode:""}
-                }
-              }
-            };
-          plan.travelplanobject.push(plan.newEdge);
-          plan.travelplanobject.push(plan.newNode);
-    }
-    plan.subNode = function () {
-      plan.travelplanobject.splice(plan.travelplanobject.length-1,1);
-      plan.travelplanobject.splice(plan.travelplanobject.length-1,1);
+    plan.newEdge = {
+      type:"transit",
+      childServices:
+      {
+        booking:{
+          requested:{mode:""}
+        }
+      }
+    };
+    plan.travelplanobject.push(plan.newEdge);
+    plan.travelplanobject.push(plan.newNode);
+  }
+  plan.subNode = function () {
+    plan.travelplanobject.splice(plan.travelplanobject.length-1,1);
+    plan.travelplanobject.splice(plan.travelplanobject.length-1,1);
 
-    }
+  }
 
 }
