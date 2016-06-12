2fdf0fe24c5263e5f357c27d9569278a92e0bbec
lakshay bansal <blakshay93@gmail.com>
2016-06-12 16:55:30 +0530

    Added the edge view

--- public/components/travelBookingParentComponent/travelBookingParentComponent.js
+++ public/components/travelBookingParentComponent/travelBookingParentComponent.js
@@ -20,7 +20,7 @@ function travelBookingParentCtrl(mainService, $location) {
   console.log(mainService.serviceData);
   travelBookingParentCtrl.$onInit = function () {
     console.log(" I am inside on init");
-    travelBookingParentCtrl.travelPlanObject = mainService.getTravelPlanObject();
+    travelBookingParentCtrl.travelPlanObject = mainService.getTravelPlanObjectInitial();
     console.log(travelBookingParentCtrl.travelPlanObject);
     travelBookingParentCtrl.currentSelectedObj = travelBookingParentCtrl.travelPlanObject[0];
     console.log(travelBookingParentCtrl.currentSelectedObj);
